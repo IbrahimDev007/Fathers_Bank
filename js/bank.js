cf03b3420158461b19d35bb77fea9083d916b9d6
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-13 16:42:32 +0600

    done withdraw and deposit and fix by is NaN

--- js/bank.js
+++ js/bank.js
@@ -1,24 +1,28 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
     const depositAmount = document.getElementById("deposit-Field").value;
-    if (depositAmount != "number") {
+    let deposit =parseFloat(depositAmount);
+
+    if (isNaN(deposit)) {
+        console.log(paparseFloat(depositAmount) );
         alert("Deposit Number is not a number")
     }
     else {
-        document.getElementById("deposit-amount").innerText = parseInt(depositAmount);
+        
+        document.getElementById("deposit-amount").innerText = parseFloat(document.getElementById('deposit-amount').innerText) +parseFloat(depositAmount);
 
-        document.getElementById("Balance-amount").innerText = parseInt(document.getElementById('Balance-amount').innerText) + parseInt(depositAmount);
+        document.getElementById("Balance-amount").innerText =parseFloat(document.getElementById('Balance-amount').innerText) +parseFloat(depositAmount);
     }
     document.getElementById("deposit-Field").value = ""
 })
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
     const withdrawAmount = document.getElementById("withdraw-Field").value;
-    if (withdrawAmount != "number") {
-        alert("Deposit Number is not a number")
+    if (isNaN(paparseFloat(withdrawAmount) )) {
+        alert("Withdraw Number is not a number")
     }
     else {
-        document.getElementById("withdraw-amount").innerText = parseInt(withdrawAmount);
-        document.getElementById("Balance-amount").innerText = parseInt(document.getElementById('Balance-amount').innerText) - parseInt(withdrawAmount);
+        document.getElementById("withdraw-amount").innerText = parseFloat(document.getElementById('withdraw-amount').innerText) +parseFloat(withdrawAmount);
+        document.getElementById("Balance-amount").innerText =parseFloat(document.getElementById('Balance-amount').innerText) -parseFloat(withdrawAmount);
     }
     document.getElementById("withdraw-Field").value = ""
 
