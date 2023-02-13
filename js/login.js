4301da185bde1eb8a589b22e8e87fff0afbe92c6
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-13 12:08:12 +0600

    git deposit and withdraw input done and js folder change

--- js/login.js
+++ js/login.js
@@ -2,7 +2,8 @@ document.getElementById("btn-submit").addEventListener('click',function (event) 
    mail=document.getElementById('emailField').value;
    pass=document.getElementById('passField').value;
    if(mail ==="sons@mail.com" && pass === "1234"){
-
+   window.location.href="./bank.html";
+   console.log(mail,pass);
    }
    else if(mail === " "&& mail ===""){
     alert("dont use empty string")
